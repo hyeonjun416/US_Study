b830d939c15c32b8b8aa998e7d40b4cb715d4700
kwakmu18 <mckkk119@naver.com>
2022-01-15 02:13:46 +0900

    return

--- Baekjoon_with_Cpp/1/cat.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    cout << "\\" << "    " << "/" << "\\" <<endl;
-    cout << " )" << "  " << "( ')\n(  /  )\n \\(__)|" << endl;
-    return 0;
-}
