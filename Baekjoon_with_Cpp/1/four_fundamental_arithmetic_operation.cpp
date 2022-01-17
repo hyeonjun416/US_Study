7b7eb35ae3881c9d8630cfc7417df237f216d803
Defi <defi622@naver.com>
2022-01-17 23:43:47 +0900

    Day 4

---
+++ Baekjoon_with_Cpp/1/four_fundamental_arithmetic_operation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "A : ";
+    cin >> a;
+    cout << "B : ";
+    cin >> b;
+    cout << endl;
+
+    cout << "A + B = " << a+b << endl;
+    cout << "A - B = " << a-b << endl;
+    cout << "A x B = " << a*b << endl;
+    cout << "A / B= " << a/b << endl;
+    cout << "A % B = " << a%b << endl;
+
+    return 0;
+
+}
