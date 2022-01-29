df1702eb707199d964d3dcb08a5f45d2b7bf9eaa
Defi <defi622@naver.com>
2022-01-29 23:18:57 +0900

    22.01.29

---
+++ Python/2.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__' : # main함수 느낌
+    a = int(input()) # input = scanf = cin
+    b = int(input()) # input은 str형으로 받기때문에 정수를 입력할 시 int를 앞에 작성
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
---
+++ Python/4.py
@@ -0,0 +1,4 @@
+n = int(input())
+
+for i in range(0,n) : # python의 반복문 형태. {} 대신 : 사용
+    print(i**2) # ** 은 제곱
