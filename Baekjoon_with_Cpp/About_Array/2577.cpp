f611c85bc98488b370b50b926f76baf13f3f6ca6
Defi <defi622@naver.com>
2022-03-12 00:45:02 +0900

    22.03.12

--- Baekjoon_with_Cpp/About_Array/2577.cpp
+++ Baekjoon_with_Cpp/About_Array/2577.cpp
@@ -1,6 +1,6 @@
 // 숫자의 개수
 #include <iostream>
-#include <cstring>
+#include <cmath>
 
 using namespace std;
 
@@ -8,6 +8,32 @@ int main(){
     int num1,num2,num3;
     cin >> num1 >> num2 >> num3;
     int multiply = num1*num2*num3;
-    
+    int digits=0;
+    while(multiply != 0){
+        multiply=multiply/10;
+        digits++;
+    }
+    multiply = num1*num2*num3;
+    int count_dnum[digits];
+    int count_num = 0;
+    int digits_cp = digits;
+    int a = pow(10,digits_cp-1);
+    for(int i = 0; i<digits; i++){
+        count_dnum[i] = multiply/a;
+        multiply = multiply % a;
+        digits_cp--;
+        cout << multiply << endl;
+    }
+    int result[10];
+    for(int i=0; i<10; i++){
+        for(int j = 0; j<digits; j++){
+            if(i==count_dnum[j]) count_num++;
+        }
+        result[i] = count_num;
+        count_num = 0;
+    }
+    // for(int i = 0; i<10; i++){
+    //     cout << result[i] << endl;
+    //}
     return 0;
 }
