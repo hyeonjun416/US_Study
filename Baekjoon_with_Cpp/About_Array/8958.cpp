bfe06a108a135ad4062bde325c6e5fea7e7b6ca7
Defi <defi622@naver.com>
2022-05-16 00:19:12 +0900

    Try 2

--- Baekjoon_with_Cpp/About_Array/8958.cpp
+++ Baekjoon_with_Cpp/About_Array/8958.cpp
@@ -6,6 +6,34 @@ int main()
 {
     int case_cnt;
     cin >> case_cnt;
+    string tmp;
+    int *score_res;
+    score_res = new int[case_cnt];
+    int score;
+    int index_cnt=0;
+    for(int i =0;i<case_cnt;i++)
+    {
+        score=0;
+        cin >> tmp;
+        while(1)
+        {
+            int index_tmp=0;
+            if(tmp[index_tmp]=='O')
+            {
+                score++;
+                score_res[index_cnt] = score;
+                continue;
+            }
+            else if(tmp[index_tmp]=='\0') break;
+            else
+            {
+                score=0;
+                continue;
+            }
+        }
+        index_cnt++;
+    }
+    for(int i=0; i<case_cnt; i++) cout << score_res[i] << endl;
     
     return 0;
 }
