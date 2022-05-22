f14e2a523221eba8a290877b4ba9c1684016233f
Defi <defi622@naver.com>
2022-05-23 01:03:07 +0900

    .

---
+++ Baekjoon_with_Cpp/About_Function/15596.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+long long sum(vector<int> &a)
+{
+    long long int sum=0;
+    for(int i = 0; i < a.size(); i++) sum+=a[i];
+    return sum;
+}
